62c20daaa2121c3650f0fd5c312608788366d54d
Alexis Fontaine <alexis.fontaine@isep.fr>
2018-03-08 11:40:38 +0100

    Implement `destroy` method on endpoints

--- src/endpoint/destroy.js
+++ src/endpoint/destroy.js
@@ -1,4 +1,18 @@
-async function destroy() {}
+async function destroy() {
+  const agentId = this.agentId;
+  const kit = this.kit;
+
+  // TODO: proper debug
+  console.log(`${'-'.repeat(10)} deleting agent "${agentId}".`);
+
+  return kit.client
+    .deleteAgent(agentId)
+    .then(() => this.agent = null)
+    .catch(/* istanbul ignore next */(error) => {
+      // TODO: proper error handling
+      throw error;
+    });
+}
 
 
 module.exports = destroy;
