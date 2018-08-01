519c1949fa4117f7cdb3c7b1a96c43b5732732da
alexisfontaine <alexis.fontaine@outlook.com>
2018-08-01 11:43:25 +0200

    Keep records without load feature in `endpoint.computePredictions`

--- src/endpoint/update.js
+++ src/endpoint/update.js
@@ -17,7 +17,7 @@ async function update(records, options) {
   const features = this.features;
   const end = agent.lastTimestamp;
 
-  let stream = Common.toRecordStream(records, options && options.import);
+  let stream = Common.toRecordStream(records, options && options.import, true);
   let failed = false;
 
   if (energy.period) stream = energy.origin
