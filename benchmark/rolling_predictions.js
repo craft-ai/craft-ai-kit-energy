198110bac69aa2878d9606dcfd2f6549deb344e1
Claire <cibeah.cb@gmail.com>
2018-10-12 19:52:22 +0200

    Automatic rolling preds in js en cours

--- benchmark/rolling_predictions.js
+++ benchmark/rolling_predictions.js
@@ -1,45 +1,60 @@
 require('dotenv').config();
-const { endpointPipeline } = require('./load_benchmark_uci');
-const path = require('path');
-
-const DATASET_PATH = path.join(__dirname, './data/uci_household_power_consumption.csv');
 const args = require('minimist')(process.argv.slice(2));
-
-// const { execFile } = require('child_process');
-
 const debug = require('debug');
+const fs = require('fs');
+
 const log = debug('craft-ai:kit-energy:benchmark:rolling_preds');
 log.enabled = true;
 
-const [START, STOP, TRAIN_STOP, FUTURE_SIZE ] = [args['start'], args['stop'], args['init'], args['future_size']]
+const { endpointPipeline } = require('./endpoint_pipeline');
+const path = require('path');
+const WeatherProvider = require('../src/providers/weather');
 
-const DEPTHS = [5,6,7,8,9,10,11,12]
-const LAST = TRAIN_STOP; 
-console.log()
-let child_args = []
-console.log(START, TRAIN_STOP, STOP, FUTURE_SIZE)
+const DATASET_PATH = path.join(__dirname, './data/uci_household_power_consumption.csv');
+const WEATHER_CACHE_PATH = path.join(__dirname, './data/weather_cache_uci.json');
 
-function rolling_pred (agent_id, depth, start, train_stop, stop, future_size){
-  let last = train_stop + future_size
-  while (last <= stop){
-    const indexes = [start, train_stop, last]
-    endpointPipeline(DATASET_PATH, indexes, {agent_id, depth})
-    log(`Depth ${depth}, ${Math.round((last-TRAIN_STOP)/(STOP-TRAIN_STOP)*100)} % done`);
-    last += future_size;
-    train_stop += future_size;
+const DEPTHS = [5]
+const [START, STOP, INIT, PRED_SIZE ] = [args['start'], args['stop'], args['init'], args['pred_size']]
+console.log(START, STOP, INIT, PRED_SIZE)
+const providers = [ 
+  {
+    provider: WeatherProvider,
+    options:{
+        // token: "j-utilise-le-cache",
+        token: process.env.DARK_SKY_TOKEN,
+        properties: ['temperatureLow', 'temperatureHigh'],
+        refresh: 'daily',
+        cache: {
+          load: () => require(WEATHER_CACHE_PATH),
+          save: (cache) => {
+            fs.writeFileSync(
+              WEATHER_CACHE_PATH,
+              JSON.stringify(cache, null, '  ')
+            );
+          }            
+        }
+    }
+  }
+]
+
+
+async function rolling_pred (agent_id, depth, providers, start_train, start_pred, stop, pred_size){
+  let last_pred = start_pred + pred_size
+  console.log(start_pred, last_pred, stop)
+  while (last_pred <= stop){
+    log('entering while loop')
+    const indexes = [start_train, start_pred, last_pred]
+    await endpointPipeline(DATASET_PATH, indexes, {agent_id, depth, providers})
+    log(`Depth ${depth}, ${Math.round((last_pred-INIT)/(STOP-INIT)*100)} % done`);
+    last_pred += pred_size;
+    start_pred += pred_size;
   }
 }
 
 DEPTHS.forEach(depth=>{
   const agent_id = "uci_power_" +  depth.toString()
-  log(`Launching rolling predictions for depth ${depth}`)
-  rolling_pred(agent_id, depth, start=START, train_stop=TRAIN_STOP, stop=STOP, future_size=FUTURE_SIZE)
-})
-
-
-  // child_args = ['load_benchmark_uci','--depth', depth.toString(), '--stop_train', train_stop.toString(), '--stop_pred', last.toString()];
-//   const child = execFile('node', child_args, (error, stdout, stderr) => {
-//     if (error) {
-//       throw error;
-//     }
-// });
+  log(`Launching rolling predictions for depth ${depth}`);
+  // AJOUT TIMEOUT 
+  rolling_pred(agent_id, depth, providers, start_train=START, start_pred=INIT, stop=STOP, pred_size=PRED_SIZE)
+  
+});
