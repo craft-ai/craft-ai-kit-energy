fbf800124517ec25735a2bf1ba05dc27c2324620
Claire <cibeah.cb@gmail.com>
2018-10-23 19:00:59 +0200

    rolling benchmark adapted for new data

--- benchmark/endpoint_pipeline.js
+++ benchmark/endpoint_pipeline.js
@@ -1,28 +1,27 @@
 require('dotenv').config();
 const EnergyKit = require('../src');
 const csv = require('../src/parsers/csv')
-const parse = require('csv-parse')
+const parse = require('../node_modules/csv-parse/lib')
 const path = require('path');
-const debug = require('debug');
+const debug = require('../node_modules/debug/src');
 const util = require('util');
 const fs = require('fs');
 
-const interpreter = EnergyKit.craftai.interpreter;
 const log = debug('craft-ai:kit-energy:benchmark:pipeline');
 log.enabled = true;
 
 function get_preds (path){ 
   try{
-    const data = require(path);
-    return Array.isArray(data)? data : [data];
+      const data = JSON.parse(fs.readFileSync(path, {encoding:"utf8"}));
+      return Array.isArray(data)? data : [data];
   }
   catch(error){
-    log("no former predictions found in", path)
-    return [];
+      console.log("no former predictions found in", path)
+      return [];
   }
 };
-
 async function prediction_dates (agent_id, path_to_dataset, prediction_start, prediction_stop){ 
+  console.log(prediction_start, prediction_stop)
   log(`${agent_id} : Retrieving prediction dates`)
   const dates = []
   return csv
@@ -32,68 +31,79 @@ async function prediction_dates (agent_id, path_to_dataset, prediction_start, pr
         .then(()=>[dates[0].date, dates[dates.length-1].date])
 }
 
-function endpointPipeline(path_to_dataset, indexes, {agent_id, tree_max_depth, providers} ){
-  
+function endpointPipeline(kit, path_to_dataset, indexes, {agent_id, depth, providers, exog} ){
+
+  if (exog !=undefined && !Array.isArray(exog)){
+    throw (`exog must be an array, received ${typeof exog}`)
+  }
+
   const [train_start, prediction_start, prediction_stop] = indexes
-  const save_file_path = path.join(__dirname, 'preds/'+agent_id +'_results.json');
+  const save_file_path = path.join(__dirname, './preds/'+agent_id +'_rolling_results.json');
   console.log(train_start, prediction_start, prediction_stop)
   const former_preds = get_preds(save_file_path);
        
-  const initializing_kit = EnergyKit
-      .initialize({
-        token: process.env.CRAFT_AI_TOKEN || process.env.CRAFT_TOKEN,
-        providers: providers
-      })
-
-  return Promise.all([prediction_dates(agent_id, path_to_dataset, prediction_start, prediction_stop), initializing_kit])
-                .then(values => {
-                  log(`${agent_id} : Energy kit initialized`);
-                  [[prediction_start_date, prediction_stop_date], kit] = values
-                  return kit
-                })
-                .then((kit) => {
-                  return kit.loadEndpoint(
-                  {
-                    id: agent_id,
-                    metadata: {
-                      region: '91',
-                      latitude: 48.458570,  // We consider a location in Essonne (France), near the dataset authors' workplace
-                      longitude: 2.156942   // Latitude and longitude retrieved from https://www.latlong.net
-                    },
-                    learning:{
-                      time_quantum:60,
-                      tree_max_depth: tree_max_depth
-                    },
+  return Promise.all([prediction_dates(agent_id, path_to_dataset, prediction_start, prediction_stop), kit])
+              .then(values => {
+                // log(`${agent_id} : Energy kit initialized`);
+                [[prediction_start_date, prediction_stop_date], kit] = values
+                return kit
+              })
+              .then((kit) => {
+                return kit.loadEndpoint(
+                {
+                  id: agent_id,
+                  metadata: {
+                    region: '91',
+                    latitude: 48.458570,  //Cedar Rapids, Iowa, retrieved from latlong.net
+                    longitude: 2.156942   // Latitude and longitude retrieved from https://www.latlong.net
                   },
-                  false // True or False = recreation of the endpoint's agent.
-                  )
-                  .then((endpoint) => {
-                    log(`${agent_id} Updating the endpoint with the training data ... `);
-                    return endpoint.update(path_to_dataset, {
-                      import: {
-                        from: train_start || 1,
-                        to: prediction_stop + 1 || 72,
-                        columns: ['date', 'load']
-                      }
-                    });
-                  })
-                  .then((endpoint) => {
-                    log(`${agent_id} : To compute predictions from ${prediction_start_date} to ${prediction_stop_date}`);
-                    return endpoint
-                      .retrieveRecords(prediction_start_date , prediction_stop_date)
-                      .then((records) => endpoint.computePredictions(records, null, prediction_start_date))
-                      .then((predictions)=>{
-                          let new_preds = former_preds.concat(predictions)
-                          let data = JSON.stringify(new_preds);
-                          fs.writeFileSync(save_file_path, data); 
-                    })
-                  })
+                  learning:{
+                    time_quantum:60,
+                    tree_max_depth: depth,
+                    properties : exog ? {[exog[0]]: {type : 'enum'}} : undefined
+                  },
+                },
+                false // True or False = recreation of the endpoint's agent.
+                )
+                .then((endpoint) => {
+                  log(`${agent_id} Updating the endpoint with the training data ... `);
+                  return endpoint.update(path_to_dataset, {
+                    import: {
+                      from: train_start || 1,
+                      to: prediction_stop + 1 || 72,
+                      columns: exog?  ['date', 'load'].concat(exog) : ['date', 'load']
+                    }
+                  });
                 })
-                .then(() => kit.close())
-                .catch((error) => {
-                  kit.close();
-                  throw error;
-                });
+                .then((endpoint) => {
+                  log(`${agent_id} : To compute predictions from ${prediction_start_date} to ${prediction_stop_date}`);
+                  return endpoint
+                    .retrieveRecords(prediction_start_date , prediction_stop_date)
+                    .then((records) => endpoint.computePredictions(records, null, prediction_start_date))
+                    .then((predictions)=>{
+                      let new_preds = former_preds.concat(predictions)
+                      let data = JSON.stringify(new_preds);
+                      return new Promise(resolve=>{
+                        let writeStream = fs.createWriteStream(save_file_path);
+                        writeStream.write(data, 'utf8');
+                        writeStream.on('finish', () => {  
+                            log('predictions saved');
+                            resolve();
+                        });
+                        // close the stream
+                        writeStream.end();
+                      })
+                        
+                    })
+                })
+              })
+              .then(() => kit)
+              .catch((error) => {
+                log(agent_id, ' failed, closing the kit')
+                console.log(error)
+                kit.close();
+                throw error;
+              });
   }
 
 
--- benchmark/rolling_predictions.js
+++ benchmark/rolling_predictions.js
@@ -1,125 +1,66 @@
 require('dotenv').config();
-const EnergyKit = require('../../../craft-ai-kit-energy/src');
 const args = require('minimist')(process.argv.slice(2));
+const debug = require('../node_modules/debug/src');
+const EnergyKit = require('../src');
 const fs = require('fs');
 const path = require('path');
-const debug = require('../../../craft-ai-kit-energy/node_modules/debug/src');
-const util = require('util');
-const WeatherProvider = require('../../../craft-ai-kit-energy/src/providers/weather');
+const WeatherProvider = require('../src/providers/weather');
 const { endpointPipeline } = require('./endpoint_pipeline');
 
-const log = debug('craft-ai:kit-energy:benchmark:rolling_preds');
+const log = debug('craft-ai:kit-energy:benchmark:uci:rolling_preds');
 log.enabled = true;
-// const WEATHER_CACHE_PATH = path.join(__dirname, './provider/weather_cache_cedar.json');
-let WEATHER_CACHE_PATH;
 
-const DATASET_PATH = path.join(__dirname, '../data/cedar/cedar_325367.csv');
-const save_file_path = path.join(__dirname, '../preds/cedar_325772_periodic_results.json');
-const PRED_SIZE =  7*24*4
-let dataset_path;
-let agent_id;
-let kit;
-const exog = ['day_of_period']
-// const METERS = ['2238','325546', '325607', '326306']
-// const METERS = ['2238']
-// const DEPTHS = [6,8,6,6]
-// const DEPTHS = [6]
-const periodic = [true]
+const DATASET_PATH = path.join(__dirname, './data/uci/uci_household_power_consumption.csv');
+const [DEPTH, PRED_SIZE, START, INIT, STOP] = [args['depth'],  args['delta'], args['start'],  args['init'], args['stop']];
+const WEATHER_CACHE_PATH = path.join(__dirname, './provider/weather_cache_uci.json');
+const AGENT_ID = 'uci_'+DEPTH.toString();
 
-// const start_train_indexes = [44807, 3664, 1, 3668]
+const providers = [ 
+    {
+        provider: WeatherProvider,
+        options:{
+            // token: "j-utilise-le-cache",
+            token: process.env.DARK_SKY_TOKEN,
+            properties: ['temperatureLow', 'temperatureHigh'],
+            refresh: 'daily',
+            cache: {
+                load: () => require(WEATHER_CACHE_PATH),
+                size:245280,
+                save: (cache) => {
+                    fs.writeFileSync(
+                    WEATHER_CACHE_PATH,
+                    JSON.stringify(cache, null, '  ')
+                    );
+                }
+            }
+        }
+    }
+]  
 
-const [DEPTH, START, INIT, STOP] = [args['depth'],  args['start'],  args['init'], args['stop']]
-const start_train_indexes = [START]
-// const start_pred_indexes = [167,106,86,106].map(el=>el*PRED_SIZE)
-const start_pred_indexes = [INIT].map(el=>el*PRED_SIZE)
-// const stop_train_indexes = [216,157,137,157].map(el=>el*PRED_SIZE)
-const stop_train_indexes = [STOP].map(el=>(el+1)*PRED_SIZE)
-
-
-// const [START, INIT, STOP, PRED_SIZE ] = [args['start'],  args['init'], args['stop'], args['step']]
-// console.log(START, STOP, INIT, PRED_SIZE)
-
-let providers;
-
-kit = EnergyKit
+let kit = EnergyKit
     .initialize({
-    token: process.env.CRAFT_AI_TOKEN || process.env.CRAFT_TOKEN || process.env.CRAFT_AI_TOKEN_AZURE,
+    token: process.env.CRAFT_AI_TOKEN || process.env.CRAFT_TOKEN,
     providers: providers
     })
 
-async function rolling_pred (agent_id, depth, providers, start_train, start_pred, stop, pred_size, periodic){
-    const init = start_train
-    console.log('periodic ?', periodic)
-    const options = periodic? {agent_id, depth, providers, exog} : {agent_id, depth, providers}
-    console.log(options)
-    // TO CHANGE : VERY BAD, DOES NOT HANDLE 2 FIGURED DEPTH NUMBERS // 
-    let dataset_path =  path.join(__dirname, '../data/cedar/'+agent_id.slice(0, agent_id.length-2)+'.csv')
-    console.log(dataset_path)
-    let last_pred = start_pred + pred_size
+async function rolling_pred (agent_id, depth, start_train, start_pred, stop, pred_size){
+    const first_pred = start_pred;
+    let last_pred = start_pred + pred_size;
+    const options = {agent_id, depth, providers}
     while (last_pred <= stop){
             const indexes = [start_train, start_pred, last_pred];
-            kit = await endpointPipeline(kit, dataset_path, indexes, options )
+            kit = await endpointPipeline(kit, DATASET_PATH, indexes, options)
             last_pred += pred_size;
             start_pred += pred_size;
-            log(`Depth ${depth}, Agent ${agent_id}, ${Math.trunc((last_pred-init)/(stop-init)*100)} % done`);
-        
+            log(`Depth ${depth}, Agent ${agent_id}, ${Math.trunc((last_pred-first_pred)/(stop-first_pred)*100)} % done`);
     }
     log(`Agent ${agent_id} : Stop date reached : no more predictions to compute`)
     return kit
 }
 
-function bench(meters, depths){
-    
+rolling_pred(AGENT_ID, DEPTH, START*PRED_SIZE, INIT*PRED_SIZE, STOP*PRED_SIZE, PRED_SIZE)
+.catch(error => {
+    log('Error: rolling predictions interrupted. See error message above. Closi');
+    return process.exit(1)
 
-    return Promise.all(
-        meters.map((meter,idx)=>{
-            WEATHER_CACHE_PATH = path.join(__dirname, '../data/provider/weather_cache_cedar.json');
-            providers = [ 
-                {
-                    provider: WeatherProvider,
-                    options:{
-                        // token: "j-utilise-le-cache",
-                        token: process.env.DARK_SKY_TOKEN,
-                        properties: ['temperature'],
-                        refresh: 'hourly',
-                        cache: {
-                            load: () => require(WEATHER_CACHE_PATH),
-                            size:245280,
-                            save: (cache) => {
-                              fs.writeFileSync(
-                                WEATHER_CACHE_PATH,
-                                JSON.stringify(cache, null, '  ')
-                             );
-                            }
-                        }
-                    }
-                }
-            ]            
-
-            let kits =
-                periodic.map(period => {
-                    depth = depths[idx]
-                    agent_id = period ? "cedar_"+meter+"_periodic_"+depth.toString() : "cedar_"+meter+'_'+depth.toString()
-                    log(`Launching rolling predictions for  ${agent_id}`);
-                    return rolling_pred(agent_id, depth, providers, 
-                        start_train=start_train_indexes[idx], 
-                        start_pred=start_pred_indexes[idx], 
-                        stop=stop_train_indexes[idx] +1, pred_size=PRED_SIZE,
-                        period)
-                })
-            return Promise.all(kits)
-            .catch((err)=>{
-                console.log(err)
-                })
-    }))
-    .then(kits => {
-        kits[0][0].close()
-        log("Benchmark done")
-    })
-    .catch((error)=>{
-        console.log(error)
-
-    })
-}
-
-bench([METER], [DEPTH])
+});
