c38f4b522e34d12be203db9fe3adbb3592f00464
Claire <cibeah.cb@gmail.com>
2018-11-02 18:44:24 +0100

    Rolling pred uci for depth 5/6, Rolling pred depth 10 and final bencgg for uci resampled to 30 min

--- benchmark/rolling_predictions.js
+++ benchmark/rolling_predictions.js
@@ -1,21 +1,39 @@
 require('dotenv').config();
 const args = require('minimist')(process.argv.slice(2));
+const csv = require('../src/parsers/csv')
 const debug = require('../node_modules/debug/src');
 const EnergyKit = require('../src');
 const fs = require('fs');
+const luxon = require('luxon');
 const path = require('path');
 const WeatherProvider = require('../src/providers/weather');
 const { endpointPipeline } = require('./endpoint_pipeline');
 
 const log = debug('craft-ai:kit-energy:benchmark:rolling_preds');
 log.enabled = true;
-
-const DATASET_PATH = path.join(__dirname, './data/uci/uci_household_power_consumption.csv');
+const DateTime = luxon.DateTime;
+const DATASET_PATH = path.join(__dirname, './data/uci/uci_30T.csv');
 const WEATHER_CACHE_PATH = path.join(__dirname, './provider/weather_cache_uci.json');
 
 const [DEPTH, PRED_SIZE, START, INIT, STOP] = [args['depth'],  args['window'], args['start'],  args['init'], args['stop']];
-const AGENT_ID = 'uci_test_'+DEPTH.toString();
+const AGENT_ID = 'uci_res_'+DEPTH.toString();
 
+
+// function prediction_dates (path_to_dataset, prediction_start, prediction_stop){ 
+//     console.log(prediction_start, prediction_stop)
+//     log(` : Retrieving prediction dates`)
+//     const dates = []
+//     return csv
+//           .stream(path_to_dataset)
+//           .slice(START * PRED_SIZE, STOP * PRED_SIZE)
+//           .observe(event => dates.push(event))
+//           .then(()=>[DateTime.fromISO(dates[0].date), DateTime.fromISO(dates[dates.length-1].date)])
+//   }
+
+//   async function baba () {
+//     const [start_date, stop_date] = await prediction_dates(DATASET_PATH);
+//     console.log(start_date < stop_date)
+// }
 // const providers = [ 
 //     {
 //         provider: WeatherProvider,
@@ -102,3 +120,6 @@ rolling_pred(AGENT_ID, DEPTH, START*PRED_SIZE, INIT*PRED_SIZE, STOP*PRED_SIZE, P
     return process.exit(1)
 
 });
+
+
+
