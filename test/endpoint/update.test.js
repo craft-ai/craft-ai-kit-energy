9eba593182f9ece499cc016b3be6262f50600af3
Claire <cibeah.cb@gmail.com>
2018-11-12 18:23:27 +0100

    Grammar fix, easier comparison

--- test/endpoint/update.test.js
+++ test/endpoint/update.test.js
@@ -284,21 +284,22 @@ test('converts accumulated energy values to mean electrical loads', (t) => {
     }));
 });
 
-test('use timezone feature when provided to parse dates', (t) => {
+test('uses timezone feature when provided to parse dates', (t) => {
   const context = t.context;
   const kit = context.kit;
   const client = kit.client;
-  const offset = '+7';
+  const offset = '+07:00';
   
   const RECORDS_WITH_TIMEZONES = RECORDS
     .map((record) => ({ ...record,  [TIMEZONE] : `utc${offset}` }));
 
   return kit
     .loadEndpoint({ id:  context.endpoint.register('test'), metadata: { zone : 'Europe/Paris' } })
-    .then((endpoint) => endpoint.update (RECORDS_WITH_TIMEZONES))
+    .then((endpoint) => endpoint.update(RECORDS_WITH_TIMEZONES))
     .then((endpoint) => client.getAgentStateHistory(endpoint.agentId))
-    .then((history) => history.map((state) => t.is(parseInt(state.sample[TIMEZONE]), parseInt(offset))));
+    .then((history) => history.forEach((state) => t.is(state.sample[TIMEZONE], offset)));
 });
+
 
 function toRecords(history) {
   const state = Object.defineProperty({}, 'timezone', { writable: true });
