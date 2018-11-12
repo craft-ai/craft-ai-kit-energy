1272b3677fbe039e933f1a2760ea631438003c3f
Claire <cibeah.cb@gmail.com>
2018-11-12 18:21:23 +0100

    Merge two tests into one chain in common.test

--- test/endpoint/common.test.js
+++ test/endpoint/common.test.js
@@ -10,23 +10,15 @@ const Common = require('../../src/endpoint/common');
 test('properly formats timezone', async(t) => {
   const date = '2017-07-30T02:30:00';
 
-  // with timezone as a feature 
-  const timezones_feature = await IANA_ZONES
-    .map((zone) => Common.toRecordStream([{ date, [TIMEZONE] : zone }], {}, false))
-    .reduce((records, record) => records.concat(record))
-    .thru(buffer())
-    .thru(nth.last);
-
-  // with timezone as an option 
-  return IANA_ZONES
-    .map((zone) => Common.toRecordStream([{ date }], {}, false, zone))
-    .reduce((records, record) => records.concat(record))
-    .map((record) => ({ formatted: record[TIMEZONE], raw: record[PARSED_RECORD][DATE].zone }))
-    .thru(buffer())
-    .thru(nth.last)
-    .then((timezones) => {
-      timezones.map((timezone, idx) => t.is(timezone.formatted, timezones_feature[idx][TIMEZONE]));
-      t.snapshot(timezones);
+  return Promise.all([
+    IANA_ZONES.map((zone) => Common.toRecordStream([{ date, [TIMEZONE] : zone }], {}, false)), // with timezone as a feature
+    IANA_ZONES.map((zone) => Common.toRecordStream([{ date }], {}, false, zone))  // with timezone as an option
+  ].map((records) => records.reduce((records, record) => records.concat(record)))
+    .map((records) => records.thru(buffer()))
+    .map((records) => records.thru(nth.last)))
+    .then((records) => {
+      records[0].forEach((record, idx) => t.is(record[TIMEZONE], records[1][idx][TIMEZONE]));
+      t.snapshot(records[0].map((record) => ({ formatted: record[TIMEZONE], raw: record[PARSED_RECORD][DATE].zone })));
     });
 });
 
